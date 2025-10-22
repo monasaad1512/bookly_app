e29a4c6b0e59f3ba6288103f673d5ca46007e864
monasaad1512 <monasaad221049@gmail.com>
2025-10-22 13:56:38 +0300

    best seller item part 3

---
+++ lib/Features/home/data/presentation/views/widgets/best_seller_list_view_item.dart
@@ -0,0 +1,65 @@
+import 'package:bookly_app/constants.dart';
+import 'package:bookly_app/core/utils/assets.dart';
+import 'package:bookly_app/core/utils/styles.dart';
+import 'package:flutter/material.dart';
+
+class BestSellerListViewItem extends StatelessWidget {
+  const BestSellerListViewItem({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: SizedBox(
+        height: 130,
+        child: Row(
+          children: [
+            AspectRatio(
+              aspectRatio: 2.6 / 4,
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: Colors.red,
+                  image: DecorationImage(
+                    fit: BoxFit.fill,
+                    image: AssetImage(AssetsData.testImage),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(width: 30),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                SizedBox(
+                  width: MediaQuery.of(context).size.width * 0.5,
+                  child: Text(
+                    'Hary Poter and Fire Fire',
+                    maxLines: 2,
+                    overflow: TextOverflow.ellipsis,
+                    style: Styles.textStyle20.copyWith(
+                      fontFamily: kGtSectraFine,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 3),
+                Text('J.K Rowling', style: Styles.textStyle14),
+                const SizedBox(height: 3),
+                Row(
+                  children: [
+                    Text(
+                      '\$19.99',
+                      style: Styles.textStyle20.copyWith(
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/Features/home/data/presentation/views/widgets/home_view_body.dart
+++ lib/Features/home/data/presentation/views/widgets/home_view_body.dart
@@ -1,7 +1,6 @@
+import 'package:bookly_app/Features/home/data/presentation/views/widgets/best_seller_list_view_item.dart';
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/custome_app_bar.dart';
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/featured_books_list_view.dart';
-import 'package:bookly_app/constants.dart';
-import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
 
@@ -22,53 +21,6 @@ class HomeViewBody extends StatelessWidget {
         ),
         BestSellerListViewItem(),
       ],
-    );
-  }
-}
-
-class BestSellerListViewItem extends StatelessWidget {
-  const BestSellerListViewItem({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 16),
-      child: SizedBox(
-        height: 130,
-        child: Row(
-          children: [
-            AspectRatio(
-              aspectRatio: 2.6 / 4,
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(16),
-                  color: Colors.red,
-                  image: DecorationImage(
-                    fit: BoxFit.fill,
-                    image: AssetImage(AssetsData.testImage),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(width: 30),
-            Column(
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: Text(
-                    'Hary Poter and Fire Fire',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: Styles.textStyle20.copyWith(
-                      fontFamily: kGtSectraFine,
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ],
-        ),
-      ),
     );
   }
 }
