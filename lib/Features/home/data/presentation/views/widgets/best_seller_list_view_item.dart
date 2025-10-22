f6dc87ccd7c23bf9ff1582320f9e3114feee4c96
monasaad1512 <monasaad221049@gmail.com>
2025-10-22 14:10:14 +0300

    best seller item done

--- lib/Features/home/data/presentation/views/widgets/best_seller_list_view_item.dart
+++ lib/Features/home/data/presentation/views/widgets/best_seller_list_view_item.dart
@@ -1,7 +1,9 @@
+import 'package:bookly_app/Features/home/data/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/constants.dart';
 import 'package:bookly_app/core/utils/assets.dart';
 import 'package:bookly_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class BestSellerListViewItem extends StatelessWidget {
   const BestSellerListViewItem({super.key});
@@ -28,34 +30,38 @@ class BestSellerListViewItem extends StatelessWidget {
               ),
             ),
             const SizedBox(width: 30),
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.5,
-                  child: Text(
-                    'Hary Poter and Fire Fire',
-                    maxLines: 2,
-                    overflow: TextOverflow.ellipsis,
-                    style: Styles.textStyle20.copyWith(
-                      fontFamily: kGtSectraFine,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 3),
-                Text('J.K Rowling', style: Styles.textStyle14),
-                const SizedBox(height: 3),
-                Row(
-                  children: [
-                    Text(
-                      '\$19.99',
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.5,
+                    child: Text(
+                      'Hary Poter and Fire Fire',
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
                       style: Styles.textStyle20.copyWith(
-                        fontWeight: FontWeight.bold,
+                        fontFamily: kGtSectraFine,
                       ),
                     ),
-                  ],
-                ),
-              ],
+                  ),
+                  const SizedBox(height: 3),
+                  Text('J.K Rowling', style: Styles.textStyle14),
+                  const SizedBox(height: 3),
+                  Row(
+                    children: [
+                      Text(
+                        '\$19.99',
+                        style: Styles.textStyle20.copyWith(
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Spacer(),
+                      BookRating(),
+                    ],
+                  ),
+                ],
+              ),
             ),
           ],
         ),
