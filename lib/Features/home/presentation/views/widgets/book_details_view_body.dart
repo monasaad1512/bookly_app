84ffd96b16a88dbd874c83b08e25603196a40d71
monasaad1512 <monasaad221049@gmail.com>
2025-10-23 14:02:14 +0300

    refactor: reorganize project architecture and folder structure

--- lib/Features/home/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/presentation/views/widgets/book_details_view_body.dart
@@ -1,6 +1,6 @@
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/books_details_section.dart';
-import 'package:bookly_app/Features/home/data/presentation/views/widgets/custome_book_details_app_bar.dart';
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/similar_books_section.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/custome_book_details_app_bar.dart';
 import 'package:flutter/material.dart';
 
 class BooksDetailsViewBody extends StatelessWidget {
--- lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
+++ lib/Features/home/presentation/views/widgets/similar_books_list_view.dart
@@ -1,4 +1,4 @@
-import 'package:bookly_app/Features/home/data/presentation/views/widgets/custome_book_image.dart';
+import 'package:bookly_app/Features/home/presentation/views/widgets/custome_book_image.dart';
 import 'package:flutter/material.dart';
 
 class SimilarBooksListView extends StatelessWidget {
