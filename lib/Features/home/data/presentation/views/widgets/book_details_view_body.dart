d0a54ecfca4b3611f40e61b6588cacfdc4893f92
monasaad1512 <monasaad221049@gmail.com>
2025-10-23 09:06:35 +0300

    add customer scroll view to book details

--- lib/Features/home/data/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/data/presentation/views/widgets/book_details_view_body.dart
@@ -12,47 +12,60 @@ class BooksDetailsViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 30),
-      child: Column(
-        children: [
-          CustomeBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.2),
-            child: CustomeBookImage(),
-          ),
-          SizedBox(height: 43),
-          Text(
-            'Book Title',
-            style: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),
-            textAlign: TextAlign.center,
-          ),
-          SizedBox(height: 6),
-          Opacity(
-            opacity: .7,
-            child: Text(
-              'Author Name',
-              style: Styles.textStyle18.copyWith(
-                fontWeight: FontWeight.w500,
-                fontStyle: FontStyle.italic,
-              ),
+
+    return CustomScrollView(
+      slivers: [
+        SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Column(
+              children: [
+                CustomeBookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+                  child: CustomeBookImage(),
+                ),
+                SizedBox(height: 43),
+                Text(
+                  'Book Title',
+                  style: Styles.textStyle30.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(height: 6),
+                Opacity(
+                  opacity: .7,
+                  child: Text(
+                    'Author Name',
+                    style: Styles.textStyle18.copyWith(
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+                BookRating(mainAxisAlignment: MainAxisAlignment.center),
+                const SizedBox(height: 37),
+                const BooksAction(),
+                Expanded(child: const SizedBox(height: 50)),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You can also like',
+                    style: Styles.textStyle14.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const SimilarBooksListView(),
+                SizedBox(height: 40),
+              ],
             ),
           ),
-          BookRating(mainAxisAlignment: MainAxisAlignment.center),
-          const SizedBox(height: 37),
-          const BooksAction(),
-          const SizedBox(height: 30),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You can also like',
-              style: Styles.textStyle14.copyWith(fontWeight: FontWeight.w600),
-            ),
-          ),
-          const SizedBox(height: 16),
-          const SimilarBooksListView(),
-        ],
-      ),
+        ),
+      ],
     );
   }
 }
