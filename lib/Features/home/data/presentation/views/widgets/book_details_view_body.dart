7943729465c91c975c743c4db0551d18e14f1a42
monasaad1512 <monasaad221049@gmail.com>
2025-10-23 09:17:05 +0300

    refactor book details view

--- lib/Features/home/data/presentation/views/widgets/book_details_view_body.dart
+++ lib/Features/home/data/presentation/views/widgets/book_details_view_body.dart
@@ -1,9 +1,6 @@
-import 'package:bookly_app/Features/home/data/presentation/views/widgets/book_rating.dart';
-import 'package:bookly_app/Features/home/data/presentation/views/widgets/books_action.dart';
+import 'package:bookly_app/Features/home/data/presentation/views/widgets/books_details_section.dart';
 import 'package:bookly_app/Features/home/data/presentation/views/widgets/custome_book_details_app_bar.dart';
-import 'package:bookly_app/Features/home/data/presentation/views/widgets/custome_book_image.dart';
-import 'package:bookly_app/Features/home/data/presentation/views/widgets/similar_books_list_view.dart';
-import 'package:bookly_app/core/utils/styles.dart';
+import 'package:bookly_app/Features/home/data/presentation/views/widgets/similar_books_section.dart';
 import 'package:flutter/material.dart';
 
 class BooksDetailsViewBody extends StatelessWidget {
@@ -11,8 +8,6 @@ class BooksDetailsViewBody extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var width = MediaQuery.of(context).size.width;
-
     return CustomScrollView(
       slivers: [
         SliverFillRemaining(
@@ -22,44 +17,10 @@ class BooksDetailsViewBody extends StatelessWidget {
             child: Column(
               children: [
                 CustomeBookDetailsAppBar(),
-                Padding(
-                  padding: EdgeInsets.symmetric(horizontal: width * 0.2),
-                  child: CustomeBookImage(),
-                ),
-                SizedBox(height: 43),
-                Text(
-                  'Book Title',
-                  style: Styles.textStyle30.copyWith(
-                    fontWeight: FontWeight.bold,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(height: 6),
-                Opacity(
-                  opacity: .7,
-                  child: Text(
-                    'Author Name',
-                    style: Styles.textStyle18.copyWith(
-                      fontWeight: FontWeight.w500,
-                      fontStyle: FontStyle.italic,
-                    ),
-                  ),
-                ),
-                BookRating(mainAxisAlignment: MainAxisAlignment.center),
-                const SizedBox(height: 37),
-                const BooksAction(),
+                const BookDetailsSection(),
+
                 Expanded(child: const SizedBox(height: 50)),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text(
-                    'You can also like',
-                    style: Styles.textStyle14.copyWith(
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                const SizedBox(height: 16),
-                const SimilarBooksListView(),
+                SimilarBooksSection(),
                 SizedBox(height: 40),
               ],
             ),
